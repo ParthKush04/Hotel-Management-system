60bafa6d86e95dacce80620973c5845f6ad624f1
Parth Kushwaha <parthkush1000@gmail.com>
2024-06-18 02:29:13 +0530

    Hotel rooms frontend

---
+++ client/src/components/Success.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Success() {
+    return (
+        <div class="alert alert-success" role="alert">
+        A simple success alert—check it out!
+      </div>  
+    )
+}
+
+export default Success
--- client/src/screens/Homescreen.jsx
+++ client/src/screens/Homescreen.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
 import Room from '../components/Room';
+import Loader from '../components/Loader';
+import Error from '../components/Error';
 
 function Homescreen() {
     const [rooms, setrooms] = useState([])
@@ -24,18 +26,20 @@ function Homescreen() {
         something();
     }, [])
     return (
-        <div>
-            <div className="row">
+        <div className='container'>
+            <div className="row justify-content-center mt-5">
                 {loading ?
-                 (
-                 <h1>loading...</h1>   
-                 ) : error ? (
-                 <h1>Error</h1>
-                 ) : (rooms.map(room => {
-                    return <div className = "col-md-9">
-                        <Room room = {room}/>
-                        </div>
-                }))}
+                    (
+                        <h1><Loader /></h1>
+                    ) : rooms.length > 1 ? (
+                        rooms.map(room => {
+                            return <div className="col-md-9 mt-3">
+                                <Room room={room} />
+                            </div>
+                        })
+                    ) : (
+                        <Error/>
+                    )}
             </div>
         </div>
     )
